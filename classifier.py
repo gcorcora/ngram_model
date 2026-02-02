50e6bf92621aa1bdf7699d79c8a8bcb924d2e56c
Gretchen Corcoran <89485480+gcorcora@users.noreply.github.com>
2026-02-02 15:44:12 -0600

    Refactor classifier.py to use aliases and improve structure

--- classifier.py
+++ classifier.py
@@ -1,16 +1,14 @@
 #n-gram LM to classify authors
 #this is master file
 
-import tiktoken
 import argparse #so can do -test
-import corpus_data
+import corpus_data as c
+import tokenizer as tk
 
 #use tiktoken with o200k encoding
 
 #command to run needs to be python3 classifier.py authorlist -test testfile and 
 #python3 classifer.py authorlist
-
-enc=tiktoken.get_encoding("o200k_base")
 
 def argument_parser() -> argparse.Namespace:
     parser = argparse.ArgumentParser(prog="classifier",
@@ -32,13 +30,36 @@ def main() -> None:
     args = argument_parser()
 
     #read in author list, get a dict with the path and the author name
-    train_corpus_dict = corpus_data.load_training_set(args.authorlist) 
-
-    
-    
-
+    train_corpus_dict = c.load_training_set(args.authorlist) 
 
     #read in testfile, if applicable
+    if args.testfile is None:
+        #if no test file, must take 10% dev set
+        train_set_dict, dev_set_dict = c.dev_train_split(train_corpus_dict)
+        
+        #tokenize + train, returning n-gram model for each author
+        #set desired n-gram length, too if wanted
+        ngram_len = 3
+        #this returns back a dict of author: {ngram_len: (ngram_count_dict, context_count_dict}
+        models = tk.train_ngram_model(train_set_dict, ngram_len)
+
+        #now classify based on dev set, print result
+        #do stupid backoff, calculate perplexity for each line in dev set per author model
+        #set up fn in tokenizer to do this
+        tk.dev_test_results(ngram_len, models, dev_set_dict)
+
+    else:
+        #train, then test on lines
+        #load train and test set
+        train_set_dict, test_lines = c.load_train_test_set(train_corpus_dict, args.testfile)
+
+        ngram_len = 3
+
+        #train ngram models
+        models = tk.train_ngram_model(train_set_dict, ngram_len)
+
+        #now classify each line in test_lines based on models
+        tk.test_file_results(ngram_len, models, test_lines)
 
 
 
